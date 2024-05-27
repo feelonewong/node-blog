a4197d8a1253e5d3e51e9011235db61fb23fa92a
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2024-05-27 08:17:12 +0800

    update: 上传27节课

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const {Article} = require('../../models')
 const router = express.Router();
-
+const {Op} = require('sequelize')
 /* GET home page. */
+// 查询
 router.get('/', async function (req, res, next) {
     try {
+        const query = req.query
+
+        const pageSize = Math.abs(Number(query.pageSize)) || 10
+        const pageNum = Math.abs(Number(query.pageNum)) || 1
+
+        const offset = (pageNum -1) * pageSize
+
+
         const condition = {
-            order: [['id', 'desc']]
+            order: [['id', 'asc']],
+            limit: pageSize,
+            offset: offset
+        }
+        if(query.title){
+            condition.where = {
+                title: {
+                    [Op.like]: `%${query.title}%`
+                }
+            }
         }
         // 数据库查询是异步的，需要async...await
-        const articles = await Article.findAll(condition)
+        const articles = await Article.findAndCountAll(condition)
         res.json({
             status: true,
             message: "查询文章成功",
             data: {
-                articles
+                total: articles.count,
+                data: articles.rows
             }
         });
     } catch (e) {
@@ -27,6 +46,7 @@ router.get('/', async function (req, res, next) {
 
 });
 
+// 查询某一个
 router.get('/:id', async function (req, res, next) {
     try {
         const {id} = req.params
@@ -56,4 +76,70 @@ router.get('/:id', async function (req, res, next) {
 
 });
 
+// 新增
+router.post('/', async function(req, res){
+    try {
+        let articles = await Article.create(req.body)
+        res.status(201).json({
+            status: true,
+            message: "文章新建成功",
+            data: {
+                articles
+            }
+        });
+    }catch (e) {
+        res.status(500).json({
+            status: true,
+            message:"新建文章失败",
+            data: e.message
+        })
+    }
+})
+
+// 删除
+router.delete("/:id", async function(req, res){
+    try {
+        const {id} = req.params
+        const articles = await Article.findByPk(id)
+        if(!articles){
+            res.status(404).json({
+                status: true,
+                message: "未查找到数据"
+            })
+        }else {
+            await articles.destroy()
+            res.status(200).json({
+                status: true,
+                message:"删除成功"
+            })
+        }
+    }catch (e) {
+        res.status(500).json({
+            status: true,
+            message:"删除失败",
+            data: e.message
+        })
+    }
+})
+
+// 更新文章
+router.put("/:id", async  function(req, res){
+    const {id} = req.params
+    const articles = await Article.findByPk(id)
+
+    if(!articles){
+        res.status(404).json({
+            status: true,
+            message: "未查询到数据"
+        })
+    }else{
+        await articles.update(req.body)
+        res.status(200).json({
+            status: true,
+            message: "更新成功"
+        })
+    }
+})
+
+// 模糊查询
 module.exports = router;
