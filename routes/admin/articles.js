52cf0b9c74f7307f0a1a8d59d53fa34768ae13ef
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2024-06-12 15:34:31 +0800

    update: finish twenty-2
    update: finish twenty-2

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {Article} = require('../../models')
 const router = express.Router();
 const {Op} = require('sequelize')
-const {NotFoundError} = require('../../utils/response')
+const {NotFoundError, SuccessReq, FailureReq} = require('../../utils/response')
 /* GET home page. */
 // 查询
 router.get('/', async function (req, res, next) {
@@ -29,20 +29,12 @@ router.get('/', async function (req, res, next) {
         }
         // 数据库查询是异步的，需要async...await
         const articles = await Article.findAndCountAll(condition)
-        res.json({
-            status: true,
-            message: "查询文章成功",
-            data: {
-                total: articles.count,
-                data: articles.rows
-            }
-        });
+        SuccessReq(res, "查询文章成功", {
+            total: articles.count,
+            data: articles.rows
+        },)
     } catch (e) {
-        res.status(500).json({
-            status: false,
-            message: "查询失败",
-            data: [e.message]
-        })
+        FailureReq(res, e)
     }
 
 });
@@ -53,19 +45,11 @@ router.get('/:id', async function (req, res, next) {
         const articles = await getArticle(req)
 
 
-        res.json({
-            status: true,
-            message: "查询文章成功",
-            data: {
-                articles
-            }
-        });
-    } catch (e) {
-        res.status(500).json({
-            status: false,
-            message: "查询失败",
-            data: [e.message]
+        SuccessReq(res, "查询文章成功", {
+            articles
         })
+    } catch (e) {
+        FailureReq(res, e)
     }
 
 });
@@ -75,21 +59,26 @@ router.post('/', async function (req, res) {
     try {
         const body = filterBody(req.body)
         let articles = await Article.create(body)
-        res.status(201).json({
-            status: true,
-            message: "文章新建成功",
-            data: {
-                articles
-            }
-        });
-    } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(e => e.message)
-            res.status(400).json({status: false, error: errors})
-        } else {
-            res.status(500).json({error: "创建失败"})
-        }
+        SuccessReq(res, "文章新建成功", {
+            articles
+        }, 201)
+    } catch (e) {
+        FailureReq(res, e)
     }
+})
+// 更新文章
+router.put("/:id", async function (req, res) {
+    try {
+        const articles = await getArticle(req)
+        const body = filterBody(req)
+        await articles.update(body)
+        SuccessReq(res, "文章更新成功", {
+            articles
+        }, 200)
+    } catch (e) {
+        FailureReq(res, e)
+    }
+
 })
 
 // 删除
@@ -97,38 +86,11 @@ router.delete("/:id", async function (req, res) {
     try {
         const articles = await getArticle(req)
         await articles.destroy()
-        res.status(200).json({
-            status: true,
-            message: "删除成功"
-        })
+        SuccessReq(res, "文章删除成功", {}, 200)
     } catch (e) {
-        res.status(500).json({
-            status: true,
-            message: "删除失败",
-            data: e.message
-        })
+        FailureReq(res, e)
     }
 })
-
-// 更新文章
-router.put("/:id", async function (req, res) {
-    const {id} = req.params
-    const articles = await Article.findByPk(id)
-    const body = filterBody(req.body)
-    if (!articles) {
-        res.status(404).json({
-            status: true,
-            message: "未查询到数据"
-        })
-    } else {
-        await articles.update(body)
-        res.status(200).json({
-            status: true,
-            message: "更新成功"
-        })
-    }
-})
-
 
 function filterBody(req) {
     return {
--- utils/response.js
+++ utils/response.js
@@ -5,6 +5,41 @@ class NotFoundError extends Error {
     }
 }
 
+function SuccessReq(res, message, data = {}, code = 200) {
+    res.status(code).json({
+        status: true,
+        message,
+        data
+    })
+
+}
+
+function FailureReq(res, error) {
+    if (error.name === 'SequelizeValidationError') {
+        const errors = error.errors.map(e => e.message)
+        return res.status(400).json({
+            status: false,
+            message: "请求参数错误",
+            errors
+        })
+    }
+    if (error.name === 'NotFoundError') {
+        const errors = error.errors.map(e => e.message)
+        return res.status(404).json({
+            status: false,
+            message: "资源不存在",
+            errors: [error.message]
+        })
+    }
+    return res.status(500).json({
+        status: false,
+        message: "服务器错误",
+        errors: [error.message]
+    })
+}
+
 module.exports = {
-    NotFoundError
+    NotFoundError,
+    SuccessReq,
+    FailureReq
 }
